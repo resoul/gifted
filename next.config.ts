6aa9e83751625af0e97800b70c57aa29255b0211
resoul <resoul@bk.ru>
2025-12-04 22:49:34 +0200

    remove motion

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@
 const nextConfig = {
 	// For local development, basePath is '/'
 	// This file will be overwritten during deployment with the appropriate basePath
-	images: {
-		domains: ["images.unsplash.com"],
-	},
 };
 
 export default nextConfig;
